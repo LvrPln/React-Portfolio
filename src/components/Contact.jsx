2d5cfb1a7b482c6f4cf157aa5e9617ed3b030b4b
LvrPln <elvirapialane16@gmail.com>
2025-04-30 20:14:22 -0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <div className="contact-info">
             <p><FaEnvelope /> <strong>Email:</strong> elvirapialane16@gmail.com</p>
             <p><FaPhone /> <strong>Phone:</strong> 09634983705</p>
-            <p><FaTiktok /> <strong>Facebook:</strong>Elvira Pialane</p>
+            <p><FaFacebook /> <strong>Facebook:</strong>Elvira Pialane</p>
             <p><FaInstagram /> <strong>Instagram:</strong> elviijp</p>
             <p><FaTiktok /> <strong>Tiktok:</strong>@Viraa</p>
             <p><FaGithub /> <strong>GitHub:</strong> LvrPn</p>
