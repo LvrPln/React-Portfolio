ecfd79e96a5f47e02f66e150bd05496ce2c6a861
LvrPln <elvirapialane16@gmail.com>
2025-04-30 20:19:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import SkillsExperience from "./components/About";
 import Contact from "./components/Contact";
 
 function App() {
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="App">
       <nav className="navbar">
-      <ul className="nav-list">
-        <li><button >Home</button></li>
-        <li><button >Skills & Experience</button></li>
-        <li><button >Contact</button></li>
-      </ul>
-    </nav>
+        <ul className="nav-list">
+          <li><button onClick={() => scrollToSection("home")}>Home</button></li>
+          <li><button onClick={() => scrollToSection("skills")}>Skills & Experience</button></li>
+          <li><button onClick={() => scrollToSection("contact")}>Contact</button></li>
+        </ul>
+      </nav>
 
-
-      <Home/>
-      <SkillsExperience />
-      <Contact />
-
+      <div id="home">
+        <Home />
+      </div>
+      <div id="skills">
+        <SkillsExperience />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
     </div>
   );
 }
